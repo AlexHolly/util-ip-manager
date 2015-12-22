8cf068ad242222dba21382f882b77451a6335a1b
Alexander Holland <alexander.holland@live.de>
2015-12-22 23:19:24 +0100

    little cleanup

--- src/main/scala/IPManager.scala
+++ src/main/scala/IPManager.scala
@@ -1,90 +1,98 @@
 package de.alexholly.util
 
 import java.net.NetworkInterface
-import java.util.Enumeration
 import java.net.InetAddress
+import java.net.URL
 import scala.xml.XML
 import scala.io.Source
-import java.net.URL
-import java.util.ArrayList
-import java.util.List
 import scala.sys.process._
 import scala.collection.JavaConversions._
+import scala.util.Try
 
 object IPManager {
 
-  //TESTS
+//TESTS
+  
 //  def main(args: Array[String]) {
 //
-//    //      println(IPManager.isIPv4("128.0.0.1"))
-//    //      println(IPManager.isIPv4("127.0.0.2"))
-//    //      println(IPManager.isIPv4("128.0.0.2"))
-//    //    println(IPManager.isIPv4("127.0.0.0"))
-//    //      println(IPManager.getLocalIP())
-//    println(IPManager.getInternetIP())
+//    //println(IPManager.isIPv4("128.0.0.1"))
+//    //println(IPManager.isIPv4("127.0.0.2"))
+//    //println(IPManager.isIPv4("128.0.0.2"))
+//    //println(IPManager.isIPv4("127.0.0.0"))
+//    //println(IPManager.getLocalIP())
+//    //println(IPManager.getInternetIP())
+//    //println(IPManager.getHostname())
+//    //println(IPManager.getAllIPs())
 //  }
 
-  //////////////////////////////////////////////////////////
 
   //TODO muss oberservable sein fuer UI
-  var ips: List[String] = new ArrayList[String]()
+  var ips: List[String] = List[String]()
 
   initIPS()
   def initIPS() {
     NetworkInterface.getNetworkInterfaces().foreach { networkInterface =>
       networkInterface.getInetAddresses.foreach { ipp =>
         val ip = ipp.getHostAddress().toString()
-        if (isIPv4(ip.toString())) {
-          ips.add(ip.toString())
+        if (isIPv4(ip)) {
+          ips :+= ip
         }
       }
     }
   }
 
+  /**
+   * @return	List of ip's as String (all available networkinterfaces )
+   */
   def getAllIPs(): List[String] = {
     ips
   }
 
+  /**
+   * @return	The name of the host(Computer) as String, if error then empty String ""
+   */
   def getHostname(): String = {
-    try {
-      return java.net.InetAddress.getLocalHost().getHostName()
-    } catch {
-      case e: java.net.UnknownHostException =>
-        return "hostname".!!
-    }
+    Try {
+      java.net.InetAddress.getLocalHost().getHostName()
+    }.getOrElse(
+      Try { "hostname".!! }.getOrElse(""))
   }
 
+  /**
+   *
+   * @return	Your local IP as String if none found "127.0.0.1"
+   */
   def getLocalIP(): String = {
-    ips.foreach { ip =>
-      if (isIPv4(ip) && !(ip.startsWith("127") || ip.endsWith(".1"))) {
-        return ip
-      }
-    }
-    return null
+    ips.filter { ip =>
+      isIPv4(ip) &&
+        !(ip.startsWith("127") ||
+          ip.endsWith(".1"))
+    }.headOption.getOrElse("127.0.0.1")
   }
 
   def isIPv4(ip: String): Boolean = {
     ip.matches("[1-9][0-9]+.[0-9]+.[0-9]+.[0-9]+")
   }
 
-  /*
+  /**
    * 		 First try, http://natip.tuxnet24.de/index.php?mode=xml
    * 		 Second, https://api.ipify.org/?format=json
-   * 		 else returns your public ip if any errors you get "127.0.0.1"
+   * 		 @returns Your public ip as String. If any errors you get "127.0.0.1"
    */
   def getInternetIP(): String = {
+    var rs = "127.0.0.1"
     try {
       val source = Source.fromURL(new URL("http://natip.tuxnet24.de/index.php?mode=xml"))
       val node = XML.loadString(source.mkString)
       var internetIP = (node \ "ip-address").text
-      return internetIP
+      rs = internetIP
     } catch {
       case e: Throwable =>
         val source = Source.fromURL(new URL("https://api.ipify.org/?format=xml"))
         var internetIP = source.mkString
-        return internetIP
+        rs = internetIP
     }
-    return "127.0.0.1"
+    rs
   }
 
 }
